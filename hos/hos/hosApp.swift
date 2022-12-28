42d273ee1c49cd50ddc1b731c3b7fb687ee41f9c
Christoph Jasper <christophjasper@minivonhristoph.speedport.ip>
2022-12-28 11:10:39 +0100

    init

---
+++ hos/hos/hosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hosApp.swift
+//  hos
+//
+//  Created by Christoph Jasper on 27.12.22.
+//
+
+import SwiftUI
+
+@main
+struct hosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
